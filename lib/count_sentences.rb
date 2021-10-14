6504373b20f938d2b1a3b8182836a451668e3282
seneiya <seneiyacooke@gmail.com>
2021-10-14 19:42:28 -0400

    commit tree d91a21510e94dd85cbcc67df5b3a2117c9a28cfd

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    self.end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    sentence = self.split(/\.|\?|\!/)
+    sentence.filter { |sentence| !sentence.empty? }.count
   end
 end
